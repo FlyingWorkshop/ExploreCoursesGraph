647f55e88c2c3622da10f724097ae963bfa14258
Logan Mondal Bhamidipaty <76822456+FlyingWorkshop@users.noreply.github.com>
2021-12-30 00:19:28 -0800

    Add files via upload

--- grapher.py
+++ grapher.py
@@ -1,4 +1,4 @@
-import re
+import random
 
 import explorecourses
 import graphviz
@@ -8,43 +8,63 @@ import utils
 
 
 class Graph:
-    def __init__(self, year: str):
+    def __init__(self, year: str, lazy=False):
         self.year = year
 
         connect = explorecourses.CourseConnection()
-        cacher.cache_course_ids(connect, year)
-        cacher.cache_departments(connect, year)
-        cacher.cache_prereqs(connect, year)
-        cacher.cache_schools(connect, year)
+        cacher.cache_course_ids(connect, year, lazy)
+        cacher.cache_departments(connect, year, lazy)
+        cacher.cache_prereqs(connect, year, lazy)
+        cacher.cache_schools(connect, year, lazy)
 
-    def graph_depts(self, *depts):
-        processed = {}
-        prereqs = utils.load_prereqs(self.year)
-        schools = utils.load_schools(self.year)
+        self._schools = utils.load_schools(self.year)
+        self._prereqs = utils.load_prereqs(self.year)
+        self._departments = utils.load_departments(self.year)
+        self._course_ids = utils.load_course_ids(self.year)
+
+    def get_dept_courses(self, dept: str):
+        school = self._schools[dept]
+        prereqs = self._prereqs[school][dept]
+        return prereqs
+
+    def get_course_name(self, course_id) -> str:
+        course_name = self._course_ids[course_id][0]  # defaults to the first name in the list of aliases
+        return course_name
+
+    @staticmethod
+    def get_color():
+        rgb = "#{:02X}{:02X}{:02X}".format(random.randrange(0, 256),
+                                           random.randrange(0, 256),
+                                           random.randrange(0, 256))
+        return rgb
+
+
+    def graph_depts(self, *depts: str, save_file=False):
+        # get data
+        graph_data = {}
         for dept in depts:
-            school = schools[dept]
-            d = self._process(prereqs[school][dept], self.year)
-            processed.update(d)
-        return self.display(processed)
+            courses = self.get_dept_courses(dept)
+            for course_id, prereq_ids in courses.items():
+                course_name = self.get_course_name(course_id)
+                if course_name not in graph_data:
+                    graph_data[course_name] = set()
+                graph_data[course_name].update([self.get_course_name(prereq_id) for prereq_id in prereq_ids])
 
-    @staticmethod
-    def _process(data: dict[str: list[str]], year: str) -> dict[str: list[str]]:
-        index = utils.load_course_ids(year)
-        processed = {}
-        for course_id, prereq_ids in data.items():
-            course_name = index[course_id][0]
-            processed[course_name] = []
-            for prereq_id in prereq_ids:
-                prereq_name = index[prereq_id][0]
-                processed[course_name].append(prereq_name)
-        return processed
-
-    @staticmethod
-    def display(processed: dict[str: str]):
-        g = graphviz.Digraph()
-        g.graph_attr.update(rankdir="LR")
-        for course_name, prereq_names in processed.items():
+        # make digraph
+        colors = {}
+        digraph = graphviz.Digraph(format="png")
+        digraph.graph_attr.update(rankdir="LR")
+        for course_name, prereq_names in graph_data.items():
+            subject = utils.get_subject(course_name)
+            if subject not in colors:
+                colors[subject] = self.get_color()
+            digraph.node(course_name, color=colors[subject], style="filled", fillcolor=colors[subject] + "32")
             for prereq_name in prereq_names:
-                g.edge(prereq_name, course_name)
-        g.render(f"output/{input('Choose a filename')}.gv")
-        return g
+                subject = utils.get_subject(course_name)
+                if subject not in colors:
+                    colors[subject] = self.get_color()
+                digraph.node(prereq_name, color=colors[subject], style="filled", fillcolor=colors[subject] + "32")
+                digraph.edge(prereq_name, course_name)
+        if save_file:
+            digraph.render(f"output/{input('Choose a filename')}.gv")
+        return digraph
